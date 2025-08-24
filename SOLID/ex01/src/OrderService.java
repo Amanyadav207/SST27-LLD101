6bc59d88267e64f97b8550d5a3cec91a698b9840
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2025-08-24 21:41:58 +0530

    solved ex01

--- SOLID/ex01/src/OrderService.java
+++ SOLID/ex01/src/OrderService.java
@@ -1,12 +1,9 @@
 public class OrderService {
-    double taxRate = 0.18;
     EmailClient email = new EmailClient();
+    TaxRate taxRate = new TaxRate();
 
-    double totalWithTax(double subtotal) {
-        return subtotal + subtotal * taxRate;
-    }
     void checkout(String customerEmail, double subtotal) {
-        double total = totalWithTax(subtotal);
+        double total = taxRate.totalWithTax(subtotal);
         email.send(customerEmail, "Thanks! Your total is " + total);
         System.out.println("Order stored (pretend DB).");
     }
---
+++ SOLID/ex01/src/TaxRate.java
@@ -0,0 +1,6 @@
+public class TaxRate {
+    double taxRate = 0.18;
+    public double totalWithTax(double subtotal) {
+        return subtotal + subtotal * taxRate;
+    }
+}
