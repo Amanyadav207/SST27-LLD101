8770e296d8d6a9da561debc5e95a896f74e8ce12
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2025-08-25 23:08:56 +0530

    ex08

--- SOLID/ex08/src/Vehicle.java
+++ SOLID/ex08/src/Vehicle.java
@@ -1,5 +1,14 @@
 public interface Vehicle {
+}
+
+interface EnginePowered {
     void startEngine();
+}
+
+interface PedalPowered {
     void pedal(int effort);
+}
+
+interface Rechargeable {
     void recharge(int kWh);
 }
